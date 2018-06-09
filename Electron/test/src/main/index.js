9c059983e170259fa4cbe4b442911839b735f20b
hairpin <vsmj012@163.com>
2018-06-09 22:52:43 +0800

    1.init: 测试项目的init 2.add: Notification,Tray

---
+++ Electron/test/src/main/index.js
@@ -0,0 +1,88 @@
+import { app, BrowserWindow } from 'electron'
+import { Notification as nnn, Tray, Menu } from 'electron'
+
+/**
+ * Set `__static` path to static files in production
+ * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
+ */
+if (process.env.NODE_ENV !== 'development') {
+    global.__static = require('path')
+        .join(__dirname, '/static')
+        .replace(/\\/g, '\\\\')
+}
+
+let mainWindow
+const winURL =
+    process.env.NODE_ENV === 'development'
+        ? `http://localhost:9080`
+        : `file://${__dirname}/index.html`
+
+function createWindow() {
+    /**
+     * Initial window options
+     */
+    mainWindow = new BrowserWindow({
+        height: 563,
+        useContentSize: true,
+        width: 1000
+    })
+
+    mainWindow.loadURL(winURL)
+
+    mainWindow.on('closed', () => {
+        mainWindow = null
+    })
+
+    var aaa = new nnn({
+        title: 'title',
+        body: '666',
+        hasReply: true
+    })
+    aaa.show()
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    if (mainWindow === null) {
+        createWindow()
+    }
+})
+
+/**
+ * Auto Updater
+ *
+ * Uncomment the following code below and install `electron-updater` to
+ * support auto updating. Code Signing with a valid certificate is required.
+ * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-electron-builder.html#auto-updating
+ */
+
+/*
+import { autoUpdater } from 'electron-updater'
+
+autoUpdater.on('update-downloaded', () => {
+  autoUpdater.quitAndInstall()
+})
+
+app.on('ready', () => {
+  if (process.env.NODE_ENV === 'production') autoUpdater.checkForUpdates()
+})
+ */
+
+let tray = null
+app.on('ready', () => {
+    tray = new Tray('/Users/mengjian/Code/logo.png')
+    const contextMenu = Menu.buildFromTemplate([
+        { label: 'Item1', type: 'radio' },
+        { label: 'Item3', type: 'radio', checked: true },
+        { label: 'Item4', type: 'radio' }
+    ])
+    tray.setToolTip('This is my application.')
+    tray.setContextMenu(contextMenu)
+})
