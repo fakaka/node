c8dd9a3577f63f557aed996653871d631cbcf244
hairpin <vsmj012@163.com>
2018-12-23 15:04:23 +0800

    1.add: getFolder 2.del: 删除 hairpin-crawler

--- hairpin-crawler/app.js
+++
@@ -1,9 +0,0 @@
-const request = require('request')
-const fs = require('fs')
-
-const mzituApp = require('./module/mzitu/app.js')
-// mzituApp.start()
-
-const insApp = require('./module/ins/app.js')
-insApp.work()
-
